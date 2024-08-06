2e483466e1abf85c47a58204c5a5c02a1c9a3b11
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-08-06 10:32:39 -0700

    Add files via upload

---
+++ assignmennt 26/script.js
@@ -0,0 +1,43 @@
+// var num = parseFloat(prompt("Enter a positive integer:"));
+// if (num > 0 && !isNaN(num)) {
+//     var rounded = Math.round(num);
+//     var floored = Math.floor(num);
+//     var ceiled = Math.ceil(num);
+
+//     document.write("Number: " + num + "<br>");
+//     document.write("Rounded Value: " + rounded + "<br>");
+//     document.write("Floor Value: " + floored + "<br>");
+//     document.write("Ceil Value: " + ceiled);
+// } else {
+//     document.write("Please enter a positive number.");
+// // }
+// var num = parseFloat(prompt("Enter a negative floting-point integer:"));
+// if (num < 0 && !isNaN(num)) {
+//     var rounded = Math.round(num);
+//     var floored = Math.floor(num);
+//     var ceiled = Math.ceil(num);
+
+//     document.write("Number: " + num + "<br>");
+//     document.write("Rounded Value: " + rounded + "<br>");
+//     document.write("Floor Value: " + floored + "<br>");
+//     document.write("Ceil Value: " + ceiled);
+// } else {
+//     document.write("Please enter a negative floating-point number.");
+// }
+// function getAbsoluteValue(number) {
+//     return number < 0 ? -number : number;
+// }
+// var number = -4; 
+// var absoluteValue = getAbsoluteValue(number);
+
+//document.write("The absolute value of " + number + " is " + absoluteValue);
+
+//   let secretNumber = 4;
+//   let userGuess = +prompt("Guess the secret number");
+//         if (userGuess === secretNumber) {
+//             console.log("Bingo! Correct answer.");
+//         } else if (userGuess + 1 === secretNumber || userGuess - 1 === secretNumber) {
+//             console.log("Close enough to the correct answer.");
+//         } else {
+//             console.log("Wrong guess. Try again!");
+//         }
